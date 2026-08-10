5e21f1f3cd948376cdca3d71d53745f846da7fc3
Enes Demirel <enesdemirelus@gmail.com>
2026-08-09 17:15:56 -0700

    fetched the words, wrote the five letter ones to a new file

---
+++ apps/api/filter_words.py
@@ -0,0 +1,15 @@
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+all_words_file = open(os.path.join(script_dir, "words_alpha.txt"))
+content = all_words_file.read().split()
+
+five_letter_words_file = open(os.path.join(script_dir, "words.txt"), 'a')
+
+for word in content:
+    if len(word) == 5:
+        five_letter_words_file.write(f"{word} \n")
+        
+print("Finished writing all the five letter words to a new file.")
+        
