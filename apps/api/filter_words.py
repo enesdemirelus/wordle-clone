6ab9c32a07f5e40d2a80ea0f272046a9becf5e0f
Enes Demirel <enesdemirelus@gmail.com>
2026-08-09 20:40:50 -0700

    changed words to more daily use ones

--- apps/api/filter_words.py
+++ apps/api/filter_words.py
@@ -2,7 +2,7 @@ import os
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
-all_words_file = open(os.path.join(script_dir, "words_alpha.txt"))
+all_words_file = open(os.path.join(script_dir, "popular.txt"))
 content = all_words_file.read().split()
 
 five_letter_words_file = open(os.path.join(script_dir, "words.txt"), 'a')
